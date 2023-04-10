2504472d89455c4832c8c6b07bd3da0c879d1516
NazikD <nazar.doroshenkov@gmail.com>
2023-04-10 13:17:46 +0300

    Create Project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam sed aperiam asperiores at illum quam, eos, repudiandae odio dolor architecto illo fugiat nam, accusantium assumenda! Eligendi ipsa aliquam doloribus ad?
+</body>
+</html>
